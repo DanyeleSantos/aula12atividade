9dbae242c6b9c62c17dac14857f19056699edb94
DanyeleSantos <135742938+DanyeleSantos@users.noreply.github.com>
2023-07-26 13:36:54 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Atividade
+{
+    internal class Produto
+    {
+        public string nome;
+        public double preco;
+        public string descricao;
+        public int quantidade;
+        public bool status;
+
+        public void mostra()
+        {
+            Console.WriteLine($" nome do produto é {nome}\n descrição {descricao}\n o é valor {preco}\n disponível {quantidade}\n em estoque o status é: {status}\n");
+        }
+
+        public void desativa()
+        {
+            quantidade= 0;
+            status= false;
+
+
+        } 
+
+        
+
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Aula12Atividade;
+
+Console.WriteLine("Hello, World!");
+
+Produto roupa = new Produto();
+roupa.nome = "vestido";
+roupa.preco = 200;
+roupa.descricao = "longo, estampado lastex ";
+roupa.quantidade = 15;
+roupa.status = true;
+
+roupa.desativa();
+roupa.mostra();
+
+
+
+Produto sapato = new Produto();
+sapato.nome = "scapin";
+sapato.preco= 150;
+sapato.descricao = "of-white, confortável c/ amarração";
+sapato.quantidade = 10;
+sapato.status = true;
+
+sapato.mostra();
+
+Produto acessorio = new Produto();
+acessorio.nome = "bolsa";
+acessorio.preco = 180;
+acessorio.descricao = "ajustável alça superior Simples elegante ";
+acessorio.quantidade = 5;
+acessorio.status = true;
+
+acessorio.mostra();
+
+
